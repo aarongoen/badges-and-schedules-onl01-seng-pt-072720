a8c1cad192119f3453b9ccfd826937abc6bbf375
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-04 13:41:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,49 +1,25 @@
 def badge_maker(name)
-  "Hello, my name is #{name}."
+  return "Hello, my name is #{name}."
 end
 
-attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
 def batch_badge_creator(attendees)
+  badges = []
   attendees.each do |name|
-    puts "badge_maker(name)"
-    # attendees.map { |name| name.to_i }
+   badges.push("Hello, my name is #{name}.")
   end
-end 
+  return badges
+end
 
-# #I'm not sure this is puts-ing out an array.
+def assign_rooms(attendees)
+  rooms = []
+  attendees.each_with_index do |name, index|
+    final = index+1
+    rooms.push("Hello, #{name}! You'll be assigned to room #{final}!")
+  end
+  return rooms
+end
 
-# batch_badge_creator(attendees) = badges
-
-# attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-# def assign_rooms(attendees, count)
-#   count = 1 
-#   while attendees <= attendees.length-1 
-#     puts "Hello, #{attendees[count]}!, 
-#     You'll be assigned to room #{count}!"
-#     count += 1
-#   end
-# end
-
-# attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-# def batch_badge_creator(attendees)
-#   count = 1 
-#   while attendees <= attendees.length-1 
-#     puts "Hello, #{attendees[count]}!, 
-#     count += 1
-#   end
-# end
-
-# def assign_rooms(attendees)
-#   %w(attendees).each_with_index { |name, index|
-#   attendees[name] = index
-#   }
-#   end
-
-# room_assignments = assign_rooms(attendees) 
-
-# def printer(attendees)
-#   puts badges
-#   puts room_assignments
-# end 
-# end
+def printer
+  puts batch_badge_creator
+  puts assign_rooms
+end
